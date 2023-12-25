9b21b798f3c852a85bf60bdb62ff318e610743ef
GitHub Action <action@github.com>
2023-12-25 11:08:34 +0000

    Initialize template repository proper
    Set package name to com.github.5than.overlay
    Set project name to overlay
    Set modid to overlay

--- src/main/java/com/github/5than/overlay/ExampleMod.java
+++ src/main/java/com/github/5than/overlay/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.5than.overlay;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "overlay", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
